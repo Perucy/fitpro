47571db037c20c78137015992ca53a6c095658a1
Perucy <perucymussiba@gmail.com>
2025-09-06 01:43:21 -0400

    login screen

---
+++ app/(login)/index.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+    View, 
+    StyleSheet,
+    Dimensions,
+    StatusBar,
+    TouchableOpacity,
+    Button,
+    Alert,
+    Text,
+    TouchableWithoutFeedback,
+} from 'react-native';
+import { VideoView, useVideoPlayer } from 'expo-video';
+
+const { width, height } = Dimensions.get('window');
+
+export default function AppPage() {
+    const player = useVideoPlayer(
+        require('../../assets/videos/FitPro_Open_App (1).mp4'), player => {
+        player.loop = true;
+        player.muted = true;
+        player.playbackRate = 1.0;
+        player.play();
+    });
+    
+    return (
+        <View style={styles.container}>
+            <StatusBar barStyle="light-content" />
+            <VideoView
+                style={styles.backgroundVideo}
+                player={player}
+                allowsFullscreen={false}
+                allowsPictureInPicture={false}
+                showsTimecodes={false}
+                requiresLinearPlayback={false}
+                contentFit="cover"
+                nativeControls={false}
+                pointerEvents="none"
+            />
+            <TouchableOpacity style={styles.overlayButton}>
+                <Text style={styles.buttonText}>Login/ Sign Up</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000'
+    },
+
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        width: width,
+        height: height,
+    },
+
+    overlayButton: {
+        position: 'absolute',
+        bottom: 80,
+        left: 40,
+        right: 40,
+        padding: 10,
+        backgroundColor:'#A6703A',
+        borderRadius:10,
+        alignItems: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+});
