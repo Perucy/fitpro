723a1a45eb6925d7269b7d0b06c330349bb15669
Perucy <perucymussiba@gmail.com>
2025-09-09 22:37:56 -0400

    started the whoop and spotify link

---
+++ app/(login)/link.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import {
+    View,
+    Text, 
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    Alert,
+    StatusBar,
+    ImageBackground,
+    KeyboardAvoidingView,
+    Platform,
+    Dimensions
+} from 'react-native';
+import { router } from 'expo-router';
+
+export default function AccLink() {
+    
+}
